bd39934379101bd4b475c1c280bd7313eaf5e2c7
yangyagami <a651685099@163.com>
2023-05-08 20:35:05 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf $PWD/kitty $HOME/.config/kitty
+ln -sf $PWD/coc-settings.json $HOME/.vim/coc-settings.json
+ln -sf $PWD/picom.conf $HOME/.config/picom/picom.conf
+ln -sf $PWD/vimrc $HOME/.vimrc
