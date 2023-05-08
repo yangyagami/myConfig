71ccf9a78a182227b26b3c50b24c3f265dd7307e
yangyagami <a651685099@163.com>
2023-05-08 20:36:32 +0800

    add ranger

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 ln -sf $PWD/kitty $HOME/.config/kitty
-ln -sf $PWD/coc-settings.json $HOME/.vim/coc-settings.json
+ln -sf $PWD/ranger $HOME/.config/ranger
+
 ln -sf $PWD/picom.conf $HOME/.config/picom/picom.conf
+ln -sf $PWD/coc-settings.json $HOME/.vim/coc-settings.json
+
 ln -sf $PWD/vimrc $HOME/.vimrc
