ed2c00b67397c686902007624a2e70e2749336a8
yangyagami <a651685099@163.com>
2023-07-09 13:30:48 +0800

    change float highlight

--- nvim/lua/config/cmd.lua
+++ nvim/lua/config/cmd.lua
@@ -1,3 +1,3 @@
 vim.cmd.colorscheme("catppuccin")
-vim.cmd [[highlight NormalFloat guibg=#1e1e2e]]
-vim.cmd [[highlight FloatBorder guifg=white guibg=#1e1e2e]]
+vim.cmd [[highlight! link NormalFloat Normal]]
+vim.cmd [[highlight! link FloatBorder Normal]]
