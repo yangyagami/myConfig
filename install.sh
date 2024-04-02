1460aaa9b961c620466efde481173740a3d5bb9a
yangsiyu <a651685099@163.com>
2024-04-02 22:14:42 +0800

    remove kitty

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/vim $HOME/.vim
 ln -sf $PWD/joshuto $HOME/.config/
-ln -sf $PWD/kitty.conf $HOME/.config/kitty/
 ln -sf $PWD/emacs.d $HOME/.emacs.d
