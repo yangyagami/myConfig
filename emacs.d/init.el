437308acacd9ef4ee59dcd8daaad79f22d32d075
yangsiyu <a651685099@163.com>
2024-04-03 19:10:52 +0800

    Edit emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -31,13 +31,14 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (display-time-mode 1)
+(setq-default c-basic-offset 8)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes))
+ '(package-selected-packages '(doom-modeline doom-themes))
  '(tool-bar-mode nil))
 
 (custom-set-faces
@@ -46,3 +47,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight medium :height 143 :width normal)))))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
