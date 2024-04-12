2fb8e68fee1c779f92d38539ff996f3c45d33d0a
yangsiyu <a651685099@163.com>
2024-04-12 20:09:03 +0800

    Add emacs config again.

---
+++ emacs.d/elpa/doom-modeline-20240411.103/doom-modeline-pkg.el
@@ -0,0 +1,17 @@
+(define-package "doom-modeline" "20240411.103" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "29.1.4.2")
+    (nerd-icons "0.1.0")
+    (shrink-path "0.3.1"))
+  :commit "6bdf3d14939e0d11ddf66c890ee15f214a6b3e7f" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/nerd-icons-20240410.838/nerd-icons-pkg.el
@@ -0,0 +1,14 @@
+(define-package "nerd-icons" "20240410.838" "Emacs Nerd Font Icons Library"
+  '((emacs "24.3"))
+  :commit "085c62742326e68ac4b220635e546708d9985f97" :authors
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/rainstormstudio/nerd-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
