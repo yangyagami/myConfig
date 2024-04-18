b41a32c116ab065ec741ea03c8e65a3370d846d3
Siyu Yang <a651685099@163.com>
2024-04-18 10:53:37 +0800

    Add lisp directory.

---
+++ emacs.d/lisp/init-elpa.el
@@ -0,0 +1,5 @@
+;; 初始化melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(provide 'init-elpa)
---
+++ emacs.d/lisp/init-theme.el
@@ -0,0 +1,30 @@
+;; doom themes
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; doom modeline
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+;; icons
+(use-package all-the-icons
+  :if (display-graphic-p))
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+
+(provide 'init-theme)
