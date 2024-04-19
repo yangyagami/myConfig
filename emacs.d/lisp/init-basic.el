5ef2c53a9439e7fc926f93c432956b215fc747c6
Siyu Yang <a651685099@163.com>
2024-04-19 08:40:00 +0800

    Recommit init-basic

---
+++ emacs.d/lisp/init-basic.el
@@ -0,0 +1,40 @@
+;; 平滑滚动
+(pixel-scroll-precision-mode 1)
+
+;; whichkey
+(require 'which-key)
+(which-key-mode 1)
+
+;; 默认打开fancy-compilation
+(fancy-compilation-mode 1)
+
+;; 默认打开whitespace-mode
+(global-whitespace-mode 1)
+
+;; 关闭tool bar
+(tool-bar-mode 0)
+
+;; tabbar
+(tab-bar-mode 1)
+
+;; 在底部打开term
+(defun bterm ()
+  (interactive)
+  (let ((buf (generate-new-buffer "*bterm*")))
+    (split-window-below 30)
+    (other-window 1)
+    (switch-to-buffer buf)
+    (term-mode)
+    (term buf)))
+
+;; modeline显示时间
+(setq display-time-format "%Y-%m-%d %H:%M")
+(display-time-mode 1)
+
+;; 显示行号
+(global-display-line-numbers-mode)
+
+;; 显示列数
+(column-number-mode 1)
+
+(provide 'init-basic)
