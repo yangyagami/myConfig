f1bc110ea43f8197bd130543cf24e909098e5d3c
Siyu Yang <a651685099@163.com>
2024-06-03 09:50:58 +0800

    Set default vterm-shell.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -38,13 +38,18 @@
  '(global-display-line-numbers-mode t)
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(counsel swiper ivy lsp-ui lsp-mode vterm cmake-mode rainbow-delimiters magit-svn google-c-style dashboard ace-window which-key flycheck company qml-mode fancy-compilation all-the-icons-gnus all-the-icons-nerd-fonts all-the-icons-dired melpa-upstream-visit magit doom-modeline doom-themes)))
+   '(counsel swiper ivy lsp-ui lsp-mode vterm cmake-mode rainbow-delimiters magit-svn google-c-style dashboard ace-window which-key flycheck company qml-mode fancy-compilation all-the-icons-gnus all-the-icons-nerd-fonts all-the-icons-dired melpa-upstream-visit magit doom-modeline doom-themes))
+ '(vterm-shell "/bin/fish")
+ '(vterm-tramp-shells '(("docker" "/bin/bash"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "RobotoMono Nerd Font" :foundry "pyrs" :slant normal :weight medium :height 128 :width normal))))
- '(fancy-compilation-default-face ((t (:inherit ansi-color-white :background "white smoke")))))
+ '(ansi-color-bright-black ((t (:background "#dfdfdf" :foreground "dim gray"))))
+ '(fancy-compilation-default-face ((t (:inherit ansi-color-white :background "white smoke"))))
+ '(term-color-black ((t (:background "#f1f1f1" :foreground "dim gray"))))
+ '(vterm-color-black ((t (:background "#f4f4f4" :foreground "red")))))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
