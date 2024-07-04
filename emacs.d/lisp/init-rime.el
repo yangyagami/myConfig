5e61d3e3b54e2c0e5c3cd4ef9e457c332e3490b9
yangsiyu <a651685099@163.com>
2024-07-04 19:55:04 +0800

    Modify rime.

--- emacs.d/lisp/init-rime.el
+++ emacs.d/lisp/init-rime.el
@@ -5,8 +5,7 @@
 (setq rime-disable-predicates
       '(rime-predicate-after-alphabet-char-p
 	rime-predicate-after-ascii-char-p
-	rime-predicate-space-after-cc-p
-	rime-predicate-space-after-ascii-p))
+	rime-predicate-space-after-cc-p))
 
 (define-key rime-mode-map (kbd "M-j") 'rime-force-enable)
 
