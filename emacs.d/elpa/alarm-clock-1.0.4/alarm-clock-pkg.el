bd138d3f7326b59166bcd51d10e37e065568558e
Siyu Yang <a651685099@163.com>
2024-10-21 17:08:28 +0800

    Add webmode. Add alarm.

---
+++ emacs.d/elpa/alarm-clock-1.0.4/alarm-clock-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "alarm-clock" "1.0.4"
+  "Alarm Clock."
+  '((emacs "24.4"))
+  :url "https://github.com/wlemuel/alarm-clock"
+  :commit "3e43ca49804afb06477dbf7ff3d6e9824522be4b"
+  :revdesc "3e43ca49804a"
+  :keywords '("calendar" "tools" "convenience")
+  :authors '(("Steve Lemuel" . "wlemuel@hotmail.com"))
+  :maintainers '(("Steve Lemuel" . "wlemuel@hotmail.com")))
