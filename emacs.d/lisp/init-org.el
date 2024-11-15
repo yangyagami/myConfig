1b724caf8a4c3e25ce9198f23ac66e1218193fcc
Siyu Yang <a651685099@163.com>
2024-11-15 14:35:03 +0800

    org 相关功能添加

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -1,4 +1,16 @@
 (setq org-todo-keywords
       '((sequence "TODO" "DOING" "DONE")))
 
+(add-hook 'org-mode-hook (lambda ()
+			   (setq line-spacing 0.45)
+			   (visual-line-mode)
+			   (auto-fill-mode 0)))
+
+(defun my-org-insert-entry ()
+  (interactive)
+  (insert "#+TITLE: TITLE")
+  (insert "#+DATE: DATE")
+  (insert "#+AUTHOR: yangsiyu"))
+
+
 (provide 'init-org)
