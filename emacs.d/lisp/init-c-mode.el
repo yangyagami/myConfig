3f7c7f1fd8f0be85ef283588bc5be2203ad79e14
yangsiyu <a651685099@163.com>
2025-09-27 16:11:12 +0800

    chore: 修改 c mode 代码缩进

--- emacs.d/lisp/init-c-mode.el
+++ emacs.d/lisp/init-c-mode.el
@@ -1,39 +1,17 @@
 (require 'company)
 (require 'lsp-mode)
 
-;; C语言下的设置
-(defun c-lineup-arglist-tabs-only (ignored)
-  "Line up argument lists by tabs, not spaces"
-  (let* ((anchor (c-langelem-pos c-syntactic-element))
-         (column (c-langelem-2nd-pos c-syntactic-element))
-         (offset (- (1+ column) anchor))
-         (steps (floor offset c-basic-offset)))
-    (* (max steps 1)
-       c-basic-offset)))
-
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            ;; Add kernel style
-            (c-add-style
-             "linux-tabs-only"
-             '("linux" (c-offsets-alist
-                        (arglist-cont-nonempty
-                         c-lineup-gcc-asm-reg
-                         c-lineup-arglist-tabs-only))))))
-
 (add-hook 'c-mode-hook
           (lambda ()
-            (setq c-basic-offset 8)
             (setq display-fill-column-indicator-column 81)
-	    (setq indent-tabs-mode t)
 	    (setq show-trailing-whitespace t)
-	    (c-set-style "linux-tabs-only")
+	    (c-set-style "linux")
+	    (setq indent-tabs-mode nil)
 	    (local-set-key (kbd "C-c d o") 'lsp-ui-doc-toggle)
 	    (local-set-key (kbd "C-c g i") 'lsp-goto-implementation)
 	    (local-set-key (kbd "C-c g d") 'lsp-goto-type-definition)
 	    (lsp)
-	    (lsp-inlay-hints-mode)
+	    ;; (lsp-inlay-hints-mode)
 	    (flycheck-mode)
 	    (rainbow-delimiters-mode)
             (display-fill-column-indicator-mode 1)))
@@ -41,3 +19,4 @@
 (setq lsp-clients-clangd-args '("--header-insertion=never"))
 
 (provide 'init-c-mode)
+
