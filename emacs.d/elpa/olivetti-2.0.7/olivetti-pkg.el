5b471383fb596da19171aa03135f12382ddd9794
yangsiyu <a651685099@163.com>
2026-08-21 16:56:32 +0800

    feat: 添加新的包。

---
+++ emacs.d/elpa/olivetti-2.0.7/olivetti-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "olivetti" "2.0.7"
+  "Minor mode to automatically balance window margins."
+  '((emacs "24.4"))
+  :url "https://github.com/rnkn/olivetti"
+  :commit "b64c0ac8c3ec9bdc67373fa754fe493be4cc81bd"
+  :revdesc "v2.0.7-0-gb64c0ac8c3ec"
+  :keywords '("wp" "text")
+  :authors '(("Paul W. Rankin" . "rnkn@rnkn.xyz"))
+  :maintainers '(("Paul W. Rankin" . "rnkn@rnkn.xyz")))
